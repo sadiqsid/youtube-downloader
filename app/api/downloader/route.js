4dcb2e4b284681e458d3f3340d3bc21448e85abf
Sadiq Siddiqui <80396044+sadiqsid@users.noreply.github.com>
2024-08-16 09:27:57 +0400

    updated api

--- app/api/downloader/route.js
+++ app/api/downloader/route.js
@@ -20,23 +20,30 @@ import ytdl from "ytdl-core";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
-    const { searchParams } = new URL(request.url);
-    const url = searchParams.get("url");
+    try {
+        const { searchParams } = new URL(request.url);
+        const url = searchParams.get("url");
 
-    if (!url || !ytdl.validateURL(url)) {
+        if (!url || !ytdl.validateURL(url)) {
+            return NextResponse.json(
+                { error: "Invalid or missing URL" },
+                { status: 400 }
+            );
+        }
+
+        const info = await ytdl.getInfo(url);
+        const videoFormat = ytdl.filterFormats(info.formats, "video");
+        const format = ytdl.chooseFormat(videoFormat, { quality: "highest" });
+        const fileName = `${info.videoDetails.title}.${format.container}`;
+        const responseHeader = {
+            "Content-Disposition": `attachment; filename="${fileName}"`,
+        };
+
+        return NextResponse.json({ format, responseHeader, fileName });
+    } catch (error) {
         return NextResponse.json(
-            { error: "Invalid or missing URL" },
-            { status: 400 }
+            { error: "An error occurred while processing the request" },
+            { status: 500 }
         );
     }
-
-    const info = await ytdl.getInfo(url);
-    const videoFormat = ytdl.filterFormats(info.formats, "video");
-    const format = ytdl.chooseFormat(videoFormat, { quality: "highest" });
-    const fileName = `${info.videoDetails.title}.${format.container}`;
-    const responseHeader = {
-        "Content-Disposition": `attachment; filename="${fileName}"`,
-    };
-
-    return NextResponse.json({ format, responseHeader, fileName });
 }
